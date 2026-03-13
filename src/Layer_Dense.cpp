3773ef85189bf9ef30207ae62b3c1d0d1e00d201
Kevin <jekevin964@gmail.com>
2026-03-13 13:04:44 -0400

    fix: naming convention

--- src/Layer_Dense.cpp
+++ src/Layer_Dense.cpp
@@ -1,12 +1,12 @@
-#include "Dense_Layer.hpp"
+#include "Layer_Dense.hpp"
 #include "Eigen/Core"
 #include <cassert>
 
 namespace unn
 {
 Dense_Layer::Dense_Layer(unsigned int n_in, unsigned int n_out,
-                         unsigned int n_samples)
-    : n_in{n_in}, n_out{n_out}, n_samples{n_samples}
+                         unsigned int batch_size)
+    : n_in{n_in}, n_out{n_out}, batch_size{batch_size}
 {
     weights = Eigen::MatrixXd::Random(n_out, n_in);
     biases = Eigen::VectorXd::Zero(n_out);
@@ -15,7 +15,7 @@ Dense_Layer::Dense_Layer(unsigned int n_in, unsigned int n_out,
 Eigen::MatrixXd Dense_Layer::operator()(const Eigen::MatrixXd &inputs) const
 {
     bool valid_in_nrows = inputs.rows() == n_in;
-    bool valid_in_ncols = inputs.cols() == n_samples;
+    bool valid_in_ncols = inputs.cols() == batch_size;
 
     assert(
         ((valid_in_nrows && valid_in_ncols) && "inputs has invalid size"));
@@ -26,7 +26,7 @@ Eigen::MatrixXd Dense_Layer::operator()(const Eigen::MatrixXd &inputs) const
     bool valid_out_ncols;
 
     valid_out_nrows = outputs.rows() == n_out;
-    valid_out_ncols = outputs.cols() == n_samples;
+    valid_out_ncols = outputs.cols() == batch_size;
 
     assert(((valid_out_nrows && valid_out_ncols) &&
             "outputs has invalid size"));
--- src/Layer_Dense.hpp
+++ src/Layer_Dense.hpp
@@ -1,5 +1,5 @@
-#ifndef DENSE_LAYER_HPP
-#define DENSE_LAYER_HPP
+#ifndef LAYER_DENSE_HPP
+#define LAYER_DENSE_HPP
 
 #include <Eigen/Core>
 
@@ -8,7 +8,7 @@ namespace unn
 struct Dense_Layer {
     const unsigned int n_in;
     const unsigned int n_out;
-    const unsigned int n_samples;
+    const unsigned int batch_size;
 
     // Each row of `weights` represents the weight for a specific output neuron
     Eigen::MatrixXd weights;
@@ -16,11 +16,11 @@ struct Dense_Layer {
     // Column vector containing all the biases Entry `i` of biases corresponds to the bias of output neuron `i`
     Eigen::VectorXd biases;
 
-    Dense_Layer(unsigned int n_in, unsigned int n_out, unsigned int n_samples);
+    Dense_Layer(unsigned int n_in, unsigned int n_out, unsigned int batch_size);
 
     // Forward Pass
     Eigen::MatrixXd operator()(const Eigen::MatrixXd &inputs) const;
 };
 } // namespace unn
 
-#endif // !DENSE_LAYER_HPP
+#endif // !LAYER_DENSE_HPP
