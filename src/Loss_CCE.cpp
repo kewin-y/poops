39e6d2b00c5a8843d9f466693959da87c73ec16e
Kevin <jekevin964@gmail.com>
2026-04-13 19:02:26 -0400

    feat: improve cce implementation
    supports dummies values. consider: should these be separate classes?

--- src/Loss_CCE.cpp
+++ src/Loss_CCE.cpp
@@ -3,29 +3,43 @@
 #include <iostream>
 #include <string>
 
+// Clamps each entry in `mat` between 1e-7 and 1 - 1e-7
+// and takes the natural logarithm of each entry
+static Eigen::MatrixXd clip_log(const Eigen::MatrixXd &mat)
+{
+  return mat.cwiseMax(1e-7).cwiseMin(1 - 1e-7).array().log();
+}
+
 namespace unn
 {
 
 Eigen::VectorXd Loss_CCE::operator()(const Eigen::MatrixXd &y_pred, const Eigen::VectorXi &y) const
 {
-  bool valid_y_pred_rows = y_pred.rows() == y.rows();
-
+  const bool valid_y_pred_rows = y_pred.rows() == y.rows();
   assert(((valid_y_pred_rows) && "mismatch in size between y_pred and y"));
 
-  bool valid_y_range = y.minCoeff() >= 0 && y.maxCoeff() < y.rows();
-
+  const bool valid_y_range = y.minCoeff() >= 0 && y.maxCoeff() < y.rows();
   assert(((valid_y_range) && "invalid range of values exist in y"));
 
-  Eigen::VectorXd extracted(y.rows());
+  const auto y_pred_clip_log = clip_log(y_pred);
 
-  for (int i = 0; i < extracted.rows(); i++) {
-    extracted(i) = y_pred(y(i), i);
+  Eigen::VectorXd neg_result(y.rows());
+
+  for (int i = 0; i < neg_result.rows(); i++) {
+    neg_result(i) = y_pred_clip_log(y(i), i);
   }
 
-  // Values smaller than 1e-7 are replaced with 1e-7.
-  // Values larger than 1 - 1e7 are replaced with 1 - 1e-7.
-  Eigen::VectorXd clipped = extracted.cwiseMax(1e-7).cwiseMin(1 - 1e-7);
+  // Negation of Negated Result -> Result
+  return -neg_result;
+}
 
-  return -clipped.array().log();
+Eigen::VectorXd Loss_CCE::operator()(const Eigen::MatrixXd &y_pred, const Eigen::MatrixXd &y) const
+{
+  const bool valid_size = y_pred.rows() == y.rows() && y_pred.cols() == y.cols();
+  assert(((valid_size) && "mismatch in size between y_pred and y"));
+
+  const auto y_pred_clip_log = clip_log(y_pred);
+
+  return -(y * y_pred_clip_log).diagonal();
 }
 } // namespace unn
--- src/Loss_CCE.hpp
+++ src/Loss_CCE.hpp
@@ -11,7 +11,13 @@ public:
   Loss_CCE() = default;
 
   // Each column vector of `y_pred` represents the prediction values
+  // Each entry of `y` represents an "index of true probability"
   Eigen::VectorXd operator()(const Eigen::MatrixXd &y_pred, const Eigen::VectorXi &y) const;
+
+  // Each column vector of `y_pred` represents the prediction values
+  // `y` is a matrix whose rows are probability distributions; how data is formatted upon
+  // calling pandas.get_dummies (I THINK ... I HAVE NO CLUE WHAT I'M TALKING ABOUT HAHA)
+  Eigen::VectorXd operator()(const Eigen::MatrixXd &y_pred, const Eigen::MatrixXd &y) const;
 };
 
 } // namespace unn
